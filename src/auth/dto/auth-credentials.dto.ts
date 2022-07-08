cea6ac9f334337bdae3cdcff9be19c4cc10a5bbb
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-07-08 11:56:23 -0400

    auth credentials validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,10 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { IsNotEmpty } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(25)
   username: string;
 
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(25)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least one special character, one upper and one lower case letter, and one number.',
+  })
   password: string;
 }
